ae237773816f10e97fbc49ee4a3838a9a7005498
Ian Eykamp <ieykamp@olin.edu>
2021-04-23 21:01:31 -0400

    read timestamp as unsigned long

--- software/Rev1/Data/binary_to_csv.py
+++ software/Rev1/Data/binary_to_csv.py
@@ -6,8 +6,8 @@ def binary_to_csv(binary_file_location: str, csv_file_location: str) -> None:
         binary_bytes = data_file.read()
     converted_bytes = [0] * round(len(binary_bytes) / 32)
     for i in range(0, len(binary_bytes), 32):
-        converted_bytes[round(i / 32)] = struct.unpack("Hfffffff", binary_bytes[i: i + 32])
-    
+        converted_bytes[round(i / 32)] = struct.unpack("Lfffffff", binary_bytes[i: i + 32])
+
     with open(csv_file_location, 'w', newline='') as write_file:
         csv_out = csv.writer(write_file)
         csv_out.writerow(["Time since launch (ms)", "X Acceleration (m/s)", "Y Acceleration (m/s)", "Z Acceleration (m/s)", "Altitude (m)", "Pressure (Pa)", "Temp (F)", "Filtered Altitude (m)"])
