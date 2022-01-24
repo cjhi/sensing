ab4056648ed893d1ba64f2c0ec5817436ce4198e
Marc Eftimie <meftimie@olin.edu>
2022-01-23 19:52:25 -0500

    Cleaned up rev 1 code

--- software/FALL_2021_REV_1/Rev_1_Code/rev_1_code.ino
+++ software/FALL_2021_REV_1/Rev_1_Code/rev_1_code.ino
@@ -4,19 +4,25 @@
 #include <Adafruit_MPL3115A2.h>
 #include <utility/imumaths.h>
 
-  // Define sensors
-  Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
-  Adafruit_MPL3115A2 mpl;
+// Define sensors
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
+Adafruit_MPL3115A2 mpl;
 
-  // Define Sensor Constants
-  uint16_t BNO055_sample_rate = 100;
+// Puts the rocket in the calibration phase (phase 1)
+// There are 5 phases: Calibration, Pre-Launch, Launch, Detection of Apogee, Detection of 1,000 feet on descent
+int phase = 2;
 
-  int mode = 1;
-  
-  
+// Configure timers for aquiring sensor data
+unsigned long currentTime = millis();
+int calibrateTime = 500; // milliseconds
+int fetchDataTime = 500; // milliseconds
+
 void setup(void)
-{
+{  
+
   Serial.begin(115200);
+  
+  // Check if sensors are detected
   if (!bno.begin())
   {
     Serial.print("No BNO055 detected");
@@ -26,35 +32,88 @@ void setup(void)
     Serial.println("NO MPL3115A2 detected");
     while(1);
   }
+  
+  // Configure LED pins
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
 
   // Configure Sensors
   mpl.setSeaPressure(1013.26);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
-  
-  switch (mode) {
-    
+  // Chooses loop to run through depending on what the phase is set to
+  switch (phase) {
+
+    // Calibration phase
     case 1:
-      displayCalStatus();
+      while (phase == 1) {
+        if (currentTime - calibrateTime > calibrateTime) {
+          displayCalStatus();
+        }
       break;
-      
+      }
+
+    // Pre-Launch phase
     case 2:
-      fetchAccelerometerData();
-      fetchBarometerData();
+      while (phase == 2) {
+        if (currentTime - fetchDataTime > fetchDataTime) {
+          fetchAccelerometerData();
+          fetchBarometerData();
+        }
       break;
+      }
+      
   }
 
 }
 
-void initSensors() {
+// First Phase
+void displayCalStatus(void)
+{
+
+  // Get Calibration values for system, gyroscope, accelerometer, and magnetometer
+  // Calibration values go from 0 to 3, 3 meaning fully calibrated
+  // LED lights will activate when gyroscope, accelerometer, and magnetometer are fully calibrated (3 lights)
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  bno.getCalibration(&system, &gyro, &accel, &mag);
+
+  // Displays calibration values
+  Serial.print("Sys:");
+  Serial.print(system, DEC);
+  Serial.print(" G:");
+  Serial.print(gyro, DEC);
+  Serial.print(" A:");
+  Serial.print(accel, DEC);
+  Serial.print(" M:");
+  Serial.println(mag, DEC);
+
+  // LEDs will activate when sensors are calibrated
+  if (gyro == 3) {
+    digitalWrite(6, HIGH);
+  }
+
+  if (accel == 3) {
+    digitalWrite(7, HIGH);
+  }
+
+  if (mag == 3) {
+    digitalWrite(8, HIGH);
+  }
+
+  // Switches to pre-launch phase when all sensors are calibrated
+  if (system == 3 && gyro == 3 && accel == 3 && mag ==3) {
+    phase = 2;
+  }
   
 }
+int fetchAccelerometerData() {
 
-void fetchAccelerometerData() {
-  
+  // Fetches linear acceleration data (ignores gravity)
   imu::Vector<3> accelerometer_data = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
 
   // Display Accelerometer Data
@@ -65,11 +124,14 @@ void fetchAccelerometerData() {
   Serial.print(" Z: ");
   Serial.print(accelerometer_data.z());
   Serial.println();
-  
+
+  float accel_data[3] = {accelerometer_data.x(), accelerometer_data.y(), accelerometer_data.z()};
+  return(accel_data);
 }
 
 float fetchBarometerData() {
 
+  // Fetches altitude data
   float altitude = mpl.getAltitude(); // m
   Serial.print(altitude);
   Serial.println();
@@ -77,33 +139,7 @@ float fetchBarometerData() {
   
 }
 
-void displayCalStatus(void)
-{
-  /* Get the four calibration values (0..3) */
-  /* Any sensor data reporting 0 should be ignored, */
-  /* 3 means 'fully calibrated" */
-  uint8_t system, gyro, accel, mag;
-  system = gyro = accel = mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-
-  /* The data should be ignored until the system calibration is > 0 */
-  Serial.print("\t");
-  if (!system)
-  {
-    Serial.print("! ");
-  }
-
-  /* Display the individual values */
-  Serial.print("Sys:");
-  Serial.print(system, DEC);
-  Serial.print(" G:");
-  Serial.print(gyro, DEC);
-  Serial.print(" A:");
-  Serial.print(accel, DEC);
-  Serial.print(" M:");
-  Serial.println(mag, DEC);
-
-  if (system == 3 && gyro == 3 && accel == 3 && mag ==3) {
-    mode = 2;
-  }
+// TODO: Implement Kalman Filter with fetched values
+void kalmanFilter() {
+  
 }
