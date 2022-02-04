5fd8f5354fc116a16151e918b4f50205d4da34b9
Cbabe <78317842+Cbabe@users.noreply.github.com>
2022-02-04 16:55:39 -0500

    Added SD card writing

---
+++ software/FALL_2021_REV_1/Spring_2022_Main/Write_SD.h
@@ -0,0 +1,33 @@
+class Write_SD {
+
+  /*=================
+   * Global Variables
+   ==================*/
+
+  /*==============
+  * Public Methods
+  ================*/
+  public:
+
+    /* Constructor:
+     * creates maze with all unvisited locations by
+     * initializing maze[][] with all 0s*/
+     void SD_writing(String dataString){
+             // make a string for assembling the data to log:
+            // read three sensors and append to the string:
+            // open the file.
+            File dataFile = SD.open("datalog.txt", FILE_WRITE);
+          
+            // if the file is available, write to it:
+            if (dataFile) {
+              dataFile.println(dataString);
+              dataFile.close();
+              // print to the serial port too:
+              Serial.println(dataString);
+            } else {
+              // if the file isn't open, pop up an error:
+              Serial.println("error opening datalog.txt");
+            }
+            delay(100); // run at a reasonable not-too-fast speed
+     }
+};
