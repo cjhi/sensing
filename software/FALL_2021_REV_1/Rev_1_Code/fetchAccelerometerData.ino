e1ad1e1a27b1fe013d1d122a901530f809543c09
Cbabe <78317842+Cbabe@users.noreply.github.com>
2022-02-07 21:14:24 -0500

    Added different .ino files for each function

---
+++ software/FALL_2021_REV_1/Rev_1_Code/fetchAccelerometerData.ino
@@ -0,0 +1,24 @@
+float fetchAccelerometerData() {
+
+  // Fetches linear acceleration data (ignores gravity)
+  imu::Vector<3> gyroscopeData = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
+  imu::Vector<3> accelerometerData = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+
+  // Calculates the true acceleration in the upward direction
+  float x_acc=accelerometerData.x();
+  float y_acc=accelerometerData.y();
+  float z_acc=accelerometerData.z();
+  float x=gyroscopeData.x(); //roll
+  float y=gyroscopeData.y(); //pitch
+ // float z=gyroscopeData.z(); //yaw
+  float alpha_angle=x*2*Pi/360;
+  float beta_angle=y*2*Pi/360;
+  // Below equation comes from https://en.wikipedia.org/wiki/Rotation_matrix
+  float z_global=-sin(beta_angle)*x_acc+sin(alpha_angle)*cos(beta_angle)*y_acc+cos(alpha_angle)*cos(beta_angle)*z_acc;
+  Serial.print("True Z Acceleration: ");
+  Serial.print(z_global);
+  Serial.println(" m/s/s");
+
+  return(z_global);
+  
+}
