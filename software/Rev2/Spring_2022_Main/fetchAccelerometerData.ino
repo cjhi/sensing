1ffcb245f7cbd8dc26553fd8e6854d3ff086bf84
Abitamim Bharmal <abitamimbharmal@hotmail.com>
2022-03-04 18:06:58 -0500

    Add global z to IMU datapoint

--- software/Rev2/Spring_2022_Main/fetchAccelerometerData.ino
+++ software/Rev2/Spring_2022_Main/fetchAccelerometerData.ino
@@ -15,14 +15,14 @@ void fetchAccelerometerData() {
   float beta_angle=y*2*Pi/360;
   // Below equation comes from https://en.wikipedia.org/wiki/Rotation_matrix
   z_global=-sin(beta_angle)*x_acc+sin(alpha_angle)*cos(beta_angle)*y_acc+cos(alpha_angle)*cos(beta_angle)*z_acc;
-  Serial.print("True Z Acceleration: ");
-  Serial.print(z_global);
-  Serial.println(" m/s/s");
+  // Serial.print("True Z Acceleration: ");
+  // Serial.print(z_global);
+  // Serial.println(" m/s/s");
   IMU[0]=x_acc;
   IMU[1]=y_acc;
   IMU[2]=z_acc;
   IMU[3]=x;
   IMU[4]=y;
   IMU[5]=z;
-  
+  IMU[6]=z_global;
 }
