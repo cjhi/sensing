94b325f978b71da04db3d609de44499ae54e36d6
Abitamim Bharmal <abitamimbharmal@hotmail.com>
2022-03-30 17:18:32 -0400

    Working data analysis

--- software/Rev2/Testing/kalman_testing/binary_to_csv.py
+++ software/Rev2/Testing/kalman_testing/binary_to_csv.py
@@ -1,32 +1,16 @@
 import struct, csv
 
-def main():
-    binary_file_location = './flightData.txt'
-    csv_file_location = './flightData.csv'
-    binary_to_csv(binary_file_location, csv_file_location)
-    print("done!")
-    return 0
-
-
-
-def binary_to_csv(binary_file_location: str, csv_file_location: str) -> None:
+def binary_to_csv(binary_file_location: str, csv_file_location: str, num_bytes: int, format: str) -> None:
     binary_bytes = None
     with open(binary_file_location, "rb") as data_file:
         binary_bytes = data_file.read()
-        print(binary_bytes)
-
-    converted_bytes = [0] * round(len(binary_bytes) / 32)
-    for i in range(0, len(binary_bytes), 32):
-
-        converted_bytes[round(i / 32)] = struct.unpack("Lffffffff", binary_bytes[i: i + 32])
-
+    converted_bytes = [0] * round(len(binary_bytes) / num_bytes)
+    for i in range(0, len(binary_bytes), num_bytes):
+        converted_bytes[round(i / num_bytes)] = struct.unpack(format, binary_bytes[i: i + num_bytes])
 
     with open(csv_file_location, 'w', newline='') as write_file:
         csv_out = csv.writer(write_file)
-        csv_out.writerow(["Time since launch (ms)", "X Acceleration (m/s)", "Y Acceleration (m/s)", "Z Acceleration (m/s)", "Altitude (m)", "Pressure (Pa)", "Temp (F)", "Filtered Altitude (m)"])
+        csv_out.writerow(["Time since launch (ms)", "X Acceleration (m/s)", "Y Acceleration (m/s)", "Z Acceleration (m/s)", "X Orientation", "Y Orientation", "Z Orientation", "Global Z Accel", "Altitude (m)", "Latitude", "Longitude", "Phase", "Kalman Altitude", "Global Z Vel","Kalman Global Z Accel"])
         for row in converted_bytes:
-            csv_out.writerow(row)
-pass
-
-
-main()
+            if row[0] != 0:
+                csv_out.writerow(row)
